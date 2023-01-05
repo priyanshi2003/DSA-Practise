b50640ad6b9bcb27d3761f7436379e7e86ba196c
Priyanshi Nagpal <74672192+priyanshi2003@users.noreply.github.com>
2023-01-05 22:49:57 +0530

    Add files via upload

---
+++ binary_tree.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public: 
+    int data;
+    node* left;
+    node* right;
+
+    node(int d){
+        this->data=d;
+        this->left=NULL;
+        this->right= NULL;
+    }
+
+};
+
+node* buildTree(node* root){
+  cout<< "Enter the data for node "<<endl;
+  int data;
+  cin>>data;
+  root= new node(data);
+
+  if(data==-1){
+    return NULL;
+  }
+
+  cout<<"Enter data for inserting in left "<<endl;
+  root->left= buildTree(root->left);
+  cout<<"Enter data to insert in right "<<endl;
+  root->right= buildTree(root->right);
+
+  return root;
+}
+
+void levelOrderTraversal(){
+    
+}
+
+
+ int main(){
+   node* root=NULL;
+   root= buildTree(root);
+
+    return 0;
+ }
+
+
+
+ 
